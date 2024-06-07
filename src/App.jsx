75a116b2a60298a0b4b595ac285212634631a325
m-Kubacki <116362784+m-Kubacki@users.noreply.github.com>
2024-06-07 13:37:14 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Quizz from "./Components/Quizz/Quizz";
+
+const App = () => {
+	return (
+		<>
+			<Quizz></Quizz>
+		</>
+	);
+};
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+	{
+		question: "Co to jest JSX w React?",
+		option1: "Stylowanie komponentów",
+		option2: "Specjalna wersja JavaScript do renderowania",
+		option3: "Rozszerzenie JavaScript do budowania komponentów",
+		option4: "Narzędzie do testowania komponentów",
+		ans: 3,
+	},
+	{
+		question: "Ktory Hook nie jest częscią Reacta?",
+		option1: "useVisual",
+		option2: "useState",
+		option3: "useContext",
+		option4: "useRef",
+		ans: 1,
+	},
+	{
+		question: "Czym jest React?",
+		option1: "Framework JavaScript",
+		option2: "Framework PHP",
+		option3: "Framework HTML",
+		option4: "Zadne z powyzszych",
+		ans: 1,
+	},
+	{
+		question: "skrot JSX pochodzi od:",
+		option1: "JavaScript EXE",
+		option2: "JavaScript XML",
+		option3: "JavaScript Example",
+		option4: "JavaScript E",
+		ans: 2,
+	},
+	{
+		question:
+			"Które polecenie służy do stworzenia nowej aplikacji React przy użyciu Create React App?",
+		option1: "npm start",
+		option2: "npm build",
+		option3: "npx create-react-app",
+		option4: "npm init react",
+		ans: 3,
+	},
+];
